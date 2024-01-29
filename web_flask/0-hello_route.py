89b4cd67f099756f5043a11fbd563f7985e6ad34
faithnekesa <faynekesa@gmail.com>
2024-01-29 15:39:47 +0000

    Adding web flask first task

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application, listens on 0.0.0.0-port 5000"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """print a message when route / is called"""
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
